6c5ef7e6cb3e5abca1c8c954ae1a11eab489204e
brianchong01 <129910656+brianchong01@users.noreply.github.com>
2024-12-06 17:50:27 +0800

    Completed initial design of NHG project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,7 @@
+// For animation on scroll
+AOS.init();
 // Wait for document to load
 document.addEventListener("DOMContentLoaded", () => {
-  // Link to pages
-  document.getElementById("gongcha-project").onclick = function () {
-    window.open(
-      "https://www.behance.net/gallery/125676387/OrderCollectBeat-the-crowd-Redesigning-Gong-cha-app",
-      "_blank"
-    );
-  };
   // Smooth scroll to projects
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
